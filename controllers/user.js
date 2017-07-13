f24eadea81b733c1f68363469583d573cd9dd01a
Andrew Pierce <apierce1992@gmail.com>
2017-07-12 21:34:42 -0400

    YAYYYY

---
+++ controllers/user.js
@@ -0,0 +1,20 @@
+var User = require("../models/User");
+
+
+module.exports = {
+
+  get: function(query, cb) {
+    // Prepare a query to get the data we scraped,
+    // and sort starting from most recent (sorted by id num)
+    User.find(query)
+      .sort({
+        _id: -1
+      })
+      // Execute this query
+      .exec(function(err, doc) {
+        // Once finished, pass the list into the callback function
+        cb(doc);
+      });
+  }
+
+};
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,6 +8,12 @@ router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
 
   // And insert something like this instead:
+	var query = {};
+	userController.get(query, function(data) {
+      // Send the article data back as JSON
+			console.log(data);
+      //res.json(data);
+    });
 	console.log("YAYYY");
   res.json([{
   	id: 1,
