238117c487121da549cc3971f736315611290e58
Andrew Pierce <apierce1992@gmail.com>
2017-07-22 22:19:23 -0400

    added basic routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './components/header';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/profile" component={Header} />
+          <Route path="/" component={App} />
+        </Switch>
+      </div>
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
